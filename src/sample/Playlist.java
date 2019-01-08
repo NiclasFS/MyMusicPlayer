97fb2522dc3ab636adac5f993120c52f994a8ce4
RobertPetersen2 <robertpetersen2@gmail.com>
2019-01-08 11:02:30 +0100

    created a playlist with two songs in the controller

---
+++ src/sample/Playlist.java
@@ -0,0 +1,63 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Playlist {
+
+    private String playlistName;
+    private String sequence;
+
+
+
+    ArrayList<Songs> songList = new ArrayList<>();
+
+
+    // Adds a song object to the song list
+    public void addSongToPlaylist(Songs song)
+    {
+        //Getting the values from the database and storing it in the song object
+        int counter = 1;
+        DB.selectSQL("Select * from tblSongs where fldSongID = '"+song.getSongID()+"'");
+        do{
+            String data = DB.getData();
+            if (data.equals(DB.NOMOREDATA)){
+                break;
+            }else{
+                if(counter == 2){
+                    song.setPath(data);
+                }
+                if(counter == 3){
+                    song.setArtistName(data);
+                }
+                if(counter == 4){
+                    song.setTrackName(data);
+                }
+            }
+            counter ++;
+        } while(true);
+
+        //Add this song to the songList ArrayList
+        songList.add(song);
+
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public ArrayList<Songs> getSongList() {
+        return songList;
+    }
+}
