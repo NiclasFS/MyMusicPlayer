34ab452025043f4065cdc8a544f8f1dbad73d459
NiclasFS <niclasfritzsoerensen@live.dk>
2019-01-11 11:46:16 +0100

    Nu med add song to playlist

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,38 +1,45 @@
 package sample;
 
-import javafx.collections.FXCollections;
+        import javafx.collections.FXCollections;
 
-import javafx.scene.control.*;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
+        import javafx.scene.control.*;
+        import javafx.scene.control.Label;
+        import javafx.scene.control.ListView;
+        import javafx.scene.control.SelectionMode;
 
-import javafx.scene.layout.Pane;
-import javafx.scene.media.*;
-import javafx.collections.ObservableList;
+        import javafx.scene.layout.Pane;
+        import javafx.scene.media.*;
+        import javafx.collections.ObservableList;
+        import javafx.scene.control.MenuButton;
 
 
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+        import javafx.fxml.FXML;
+        import javafx.fxml.Initializable;
 
-import java.io.*;
-import java.net.*;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
-import java.util.StringTokenizer;
+        import java.io.*;
+        import java.net.*;
+        import java.time.Duration;
+        import java.util.ArrayList;
+        import java.util.ResourceBundle;
+        import java.util.StringTokenizer;
 
 public class Controller implements Initializable {
+    
     @FXML
     private MediaView mediaV;
     @FXML
-    private Button bPlay, bPause, bStop, bAddSongsToPlaylist, bAddPlaylist, bEditPlaylist, bDeletePlaylist, bContinue, bPlaylistAddOK, bCreatePlaylist;
+    private Button bPlay, bPause, bStop, bAddSongtoPlaylist, bAddPlaylist, bEditPlaylist, bDeletePlaylist;
+    @FXML
+    private Button bContinue, bPlaylistAddOK, bCreatePlaylist, bAddSongOK;
+    @FXML
+    private MenuButton mbPlaylistlist;
+
     @FXML
     private Label lbSongs, lbPlaylist, lbSongTitle;
     @FXML
-    private Pane pAddPlaylist;
+    private Pane pAddPlaylist, pAddSongToPlaylist;
     @FXML
-    private TextField tfPlaylistName;
+    private TextField tfPlaylistName, tfAddSongtoPlaylist;
     @FXML
     private ListView<Playlist> lvPlaylist;
     @FXML
@@ -43,14 +50,14 @@ public class Controller implements Initializable {
 
 
     Songs selectedSong;
-    ArrayList<Playlist> playlistsList = new ArrayList<>(); //arraylist containing all playlists
+
     //Playlist myPlaylist = new Playlist("All Songs"); //The playlist containing all songs
 
 
     // Declaration: ArrayLists
     ArrayList<Playlist> playlistsList = new ArrayList<>(); //arraylist containing all playlist
     // Declaration: Playlists
-    
+
     Playlist deleteTester = new Playlist("Delete tester");
     // Declaration Songs
 
@@ -114,8 +121,6 @@ public class Controller implements Initializable {
         }
         //Adding all song objects from "All Songs" playlist to items as Songs
         ObservableList<Songs> items = FXCollections.observableArrayList();
-        for (int i = 0; i < allSongsPlaylist.getSongList().size(); i++) {
-
 
         //song2.printValues();
         //allSongsList = myPlaylist.getSongList();
@@ -123,12 +128,12 @@ public class Controller implements Initializable {
         // System.out.println(myPlaylist.getSongList());
 
         //Adding all song objects from myPlayList to items as Songs
-        for (int i = 0; i < myPlaylist.getSongList().size(); i++) {
+        for (int i = 0; i < allSongsPlaylist.getSongList().size(); i++) {
             //adding to ArrayList in playlist object
             items.add(allSongsPlaylist.songList.get(i));
         }
         // Calling updateSonglist(); to get the list of all songs on application start
-        updateSonglist(myPlaylist);
+        updateSonglist(allSongsPlaylist);
         //Setting the items/objects in the listview
         //lvSongList.setItems(items);
         // Using CellFactory to make the ListViewer show the names of the songs not the object
@@ -149,8 +154,8 @@ public class Controller implements Initializable {
 
         //setting Playlists in listview
         updatePlaylists(playlistItems);
-
     }
+
 
     @FXML
     /**
@@ -230,38 +235,8 @@ public class Controller implements Initializable {
     }
 
 
-     //updates all playlists
-    //***To be used when the user makes changes to a playlist***
-    public void updatePlaylists(ObservableList<Playlist> playlistItems){
-        //adding playlists to observablelist
-        for (int i = 0; i < playlistsList.size(); i++) {
-            //adding to ArrayList in playlist object
-            playlistItems.add(playlistsList.get(i));
-        }
-        //Setting the items/objects in the listview
-        lvPlaylist.setItems(playlistItems);
-        lvPlaylist.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        //setting name as text in listview
-        lvPlaylist.setCellFactory(param -> new ListCell<>() {
-            @Override
-            protected void updateItem(Playlist item, boolean empty) {
-                super.updateItem(item, empty);
-
-                if (empty || item == null || item.getPlaylistName() == null) {
-                    setText(null);
-                } else {
-                    setText(item.getPlaylistName());
-                }
-            }
-        });
-
-        //setting the sequence for all playlists
-        for (Playlist element:playlistsList) {
-            setPlaylistSequence(element);
-        }
 
 
-    }
 
 
     /**
@@ -273,68 +248,68 @@ public class Controller implements Initializable {
         items.setAll(selectedPlaylist.songList);
         lvSongList.setItems(items);
     }
-        
-        // Creates a pop up window for the user input
-        @FXML
-        public void handleCreatePlaylist ()
-        {
-            pAddPlaylist.setDisable(false);
-            pAddPlaylist.setOpacity(1.0);
+
+    // Creates a pop up window for the user input
+    @FXML
+    public void handleCreatePlaylist ()
+    {
+        pAddPlaylist.setDisable(false);
+        pAddPlaylist.setOpacity(1.0);
+    }
+
+    //Gets user input and creates a playlist with the name specified by the user
+    @FXML
+    public void handleAddPlaylist ()
+    {
+
+        String playlistName = tfPlaylistName.getText();
+
+        for (int i = 0; i < playlistItems.size(); i++) {
+            //adding to ArrayList in playlist object
+            playlistsList.remove(playlistItems.get(i));
         }
 
-        //Gets user input and creates a playlist with the name specified by the user
-        @FXML
-        public void handleAddPlaylist ()
-        {
+        playlistsList.add(new Playlist(playlistName));
+        updatePlaylists(playlistItems);
 
-            String playlistName = tfPlaylistName.getText();
-
-            for (int i = 0; i < playlistItems.size(); i++) {
-                //adding to ArrayList in playlist object
-                playlistsList.remove(playlistItems.get(i));
+        //Creates a temporary object which is set to the playlist in playlislist that matches the name from the user input
+        Playlist temp = new Playlist("temp");
+        for (Playlist element : playlistsList) {
+            if (element.getPlaylistName().equals(playlistName)) {
+                temp = element;
             }
+        }
+        // Stores the playlist in the database
+        DB.insertSQL("insert into tblPlaylist values ('" + temp.getPlaylistName() + "','" + temp.getSequence() + "');");
 
-            playlistsList.add(new Playlist(playlistName));
-            updatePlaylists(playlistItems);
+        //Hides the pop up window
+        pAddPlaylist.setDisable(true);
+        pAddPlaylist.setOpacity(0.0);
 
-            //Creates a temporary object which is set to the playlist in playlislist that matches the name from the user input
-            Playlist temp = new Playlist("temp");
-            for (Playlist element : playlistsList) {
-                if (element.getPlaylistName().equals(playlistName)) {
-                    temp = element;
-                }
-            }
-                // Stores the playlist in the database
-            DB.insertSQL("insert into tblPlaylist values ('" + temp.getPlaylistName() + "','" + temp.getSequence() + "');");
+    }
 
-            //Hides the pop up window
-            pAddPlaylist.setDisable(true);
-            pAddPlaylist.setOpacity(0.0);
+    // Deletes the selected playlist
+    public void handleDeletePlaylist ()
+    {
+        Playlist selectedPlaylist = lvPlaylist.getSelectionModel().getSelectedItem();
 
+        DB.deleteSQL("Delete from tblPlaylist where fldPlaylistName = '" + selectedPlaylist.getPlaylistName() + "';");
+        //DB.deleteSQL("Delete from tblPlaylist where fldSequence = 'null';");
+        //DB.deleteSQL("delete from tblPlaylist values ('"+selectedSong+"');");
+        // System.out.println(selectedPlaylist.getPlaylistName());
+
+
+        for (int i = 0; i < playlistItems.size(); i++) {
+
+            playlistsList.remove(playlistItems.get(i));
         }
 
-       // Deletes the selected playlist
-        public void handleDeletePlaylist ()
-        {
-            Playlist selectedPlaylist = lvPlaylist.getSelectionModel().getSelectedItem();
 
-            DB.deleteSQL("Delete from tblPlaylist where fldPlaylistName = '" + selectedPlaylist.getPlaylistName() + "';");
-            //DB.deleteSQL("Delete from tblPlaylist where fldSequence = 'null';");
-            //DB.deleteSQL("delete from tblPlaylist values ('"+selectedSong+"');");
-            // System.out.println(selectedPlaylist.getPlaylistName());
+        lvPlaylist.getItems().remove(selectedPlaylist);
+        updatePlaylists(playlistItems);
 
 
-            for (int i = 0; i < playlistItems.size(); i++) {
-
-                playlistsList.remove(playlistItems.get(i));
-            }
-
-
-            lvPlaylist.getItems().remove(selectedPlaylist);
-            updatePlaylists(playlistItems);
-
-
-        }
+    }
 
     //creates a sequence for a playlist by looking at the songlist of that playlist, and stores it in tblPlaylist
     public void setPlaylistSequence(Playlist playlist){
@@ -397,7 +372,7 @@ public class Controller implements Initializable {
         }
     }
 
-    
+
 
     public void selectNextSong(){
         //check where in the sequence the selectedSong is
@@ -407,41 +382,106 @@ public class Controller implements Initializable {
     }
 
 
-        public void updatePlaylists (ObservableList < Playlist > playlistItems) {
-            //adding playlists to observablelist
-            for (int i = 0; i < playlistsList.size(); i++) {
-                //adding to ArrayList in playlist object
-                playlistItems.add(playlistsList.get(i));
-            }
-            //Setting the items/objects in the listview
-            lvPlaylist.setItems(playlistItems);
-            lvPlaylist.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-            //setting name as text in listview
-            lvPlaylist.setCellFactory(param -> new ListCell<>() {
-                @Override
-                protected void updateItem(Playlist item, boolean empty) {
-                    super.updateItem(item, empty);
+    public void updatePlaylists (ObservableList < Playlist > playlistItems) {
+        //adding playlists to observablelist
+        for (int i = 0; i < playlistsList.size(); i++) {
+            //adding to ArrayList in playlist object
+            playlistItems.add(playlistsList.get(i));
+        }
+        //Setting the items/objects in the listview
+        lvPlaylist.setItems(playlistItems);
+        lvPlaylist.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        //setting name as text in listview
+        lvPlaylist.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(Playlist item, boolean empty) {
+                super.updateItem(item, empty);
 
-                    if (empty || item == null || item.getPlaylistName() == null) {
-                        setText(null);
-                    } else {
-                        setText(item.getPlaylistName());
-                    }
+                if (empty || item == null || item.getPlaylistName() == null) {
+                    setText(null);
+                } else {
+                    setText(item.getPlaylistName());
                 }
-            });
-        }
-        @FXML
-        /**
-         * This method updates the ListView of songs to the corresponding playlist
-         */
-        private void getPlaylistInformation () {
-            // New object that gets the songs(items) from the selected playlist.
-            Playlist selectedPlaylist = lvPlaylist.getSelectionModel().getSelectedItem();
-            // Updates the ListView to show the songs(items) in the selected playlist.
-            updateSonglist(selectedPlaylist);
-
-            //System.out.println(selectedPlaylist.getSongList().size());
-            //System.out.println(selectedPlaylist.getPlaylistName());
-            //System.out.println(selectedPlaylist.getSongList().toString());
-        }
+            }
+        });
     }
+    @FXML
+    /**
+     * This method updates the ListView of songs to the corresponding playlist
+     */
+    private void getPlaylistInformation () {
+        // New object that gets the songs(items) from the selected playlist.
+        Playlist selectedPlaylist = lvPlaylist.getSelectionModel().getSelectedItem();
+        // Updates the ListView to show the songs(items) in the selected playlist.
+        updateSonglist(selectedPlaylist);
+
+        //System.out.println(selectedPlaylist.getSongList().size());
+        //System.out.println(selectedPlaylist.getPlaylistName());
+        //System.out.println(selectedPlaylist.getSongList().toString());
+    }
+
+    @FXML
+    public void addSongToPlaylist ()
+    {
+
+        MenuItem menuItem = new MenuItem("asdsg");
+
+
+        ObservableList<MenuItem> temp = FXCollections.observableArrayList();
+
+
+        mbPlaylistlist.getItems().addAll(new MenuItem("Peter"), new MenuItem("Jakob"));
+        mbPlaylistlist.getItems().add(menuItem);
+
+        //menuItem.setOnAction();
+
+        /*
+        for (int i = 0; i < 1; i++) {
+
+            mbPlaylistlist.getItems().add(menuItem);
+
+        }
+*/
+    }
+
+    // Creates a pop up window for the user input
+
+    @FXML
+    public void handleAddToPlaylistPopup() {
+        pAddSongToPlaylist.setDisable(false);
+        pAddSongToPlaylist.setOpacity(1.0);
+
+    }
+
+    /**
+     * This method checks the user input for existing playlist, and adds the selected song to the corresponding playlist
+     *
+     */
+    @FXML
+    public void handleSongAddOK ()
+    {
+        String playlistName = tfAddSongtoPlaylist.getText();
+
+        selectedSong = lvSongList.getSelectionModel().getSelectedItem();
+
+
+
+        //Creates a temporary object which is set to the playlist in playlislist that matches the name from the user input
+        for (int i = 0; i < playlistsList.size(); i++) {
+            if(playlistsList.get(i).getPlaylistName().equals(playlistName)){
+                playlistsList.get(i).addSongToPlaylist(selectedSong);
+                setPlaylistSequence(playlistsList.get(i));
+            }
+        }
+
+
+
+
+        //Hides the pop up window
+        pAddSongToPlaylist.setDisable(true);
+        pAddSongToPlaylist.setOpacity(0.0);
+
+
+    }
+
+}
